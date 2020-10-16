e90b220964d32ae36f6e26f08693d53beb584944
Jacob26502 <57222813+Jacob26502@users.noreply.github.com>
2020-10-16 16:15:19 +0100

    Update nameGetter.java

--- nameGetter.java
+++ nameGetter.java
@@ -2,4 +2,7 @@
 import java.io.*;
 import java.net.URL;
 import static java.util.stream.Collectors.*;
-public class nameGetter {public static void main(String[] args) throws IOException{ String ss= (new BufferedReader(new InputStreamReader((new URL("https://www.ecs.soton.ac.uk/people/" + new BufferedReader(new InputStreamReader(System.in)).readLine())).openStream())).lines().collect(joining()));System.out.print(ss.substring((ss.indexOf("property=\"name\">")+16),ss.indexOf("<em property=\"honorificSuffix\"")));}}
+public class nameGetter {
+  public static void main(String[] args) throws IOException{ 
+    String ss= (new BufferedReader(new InputStreamReader((new URL("https://www.ecs.soton.ac.uk/people/" + new BufferedReader(new InputStreamReader(System.in)).readLine())).openStream())).lines().collect(joining()));
+    System.out.print(ss.substring((ss.indexOf("property=\"name\">")+16),ss.indexOf("<em property=\"honorificSuffix\"")));}}
